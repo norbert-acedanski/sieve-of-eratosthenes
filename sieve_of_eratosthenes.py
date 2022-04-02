6ccb29cb83583905972044b587de7c2ac11f88a0
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-04-02 22:02:41 +0200

    Optimizing the method

--- sieve_of_eratosthenes.py
+++ sieve_of_eratosthenes.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 def primes_less_than(limit_number: int) -> list:
     if limit_number <= 2:
@@ -7,11 +8,14 @@ def primes_less_than(limit_number: int) -> list:
     numbers_list = [None]*limit_number
     numbers_list[0] = False
     numbers_list[1] = False
-    for number in range(2, limit_number):
+    for number in range(2, math.isqrt(limit_number)):
         if numbers_list[number] == None:
             numbers_list[number] = True
             for multiple in range(number + number, limit_number, number):
                 numbers_list[multiple] = False
+    for number in range(math.isqrt(limit_number) + 1, limit_number):
+        if numbers_list[number] == None:
+            numbers_list[number] = True
     return [number for number in range(limit_number) if numbers_list[number]]
 
 if __name__ == "__main__":
