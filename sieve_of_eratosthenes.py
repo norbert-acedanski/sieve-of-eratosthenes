a28b6e07513dcc822c9f3c216779574d0c292ebd
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-11-30 22:56:51 +0100

    Refactor

--- sieve_of_eratosthenes.py
+++ sieve_of_eratosthenes.py
@@ -1,5 +1,6 @@
 import math
 
+
 def primes_less_than(limit_number: int) -> list:
     if limit_number <= 2:
         raise ValueError("Input number greater than 2!")
@@ -7,14 +8,15 @@ def primes_less_than(limit_number: int) -> list:
     numbers_list[0] = False
     numbers_list[1] = False
     for number in range(2, math.isqrt(limit_number)):
-        if numbers_list[number] == None:
+        if numbers_list[number] is None:
             numbers_list[number] = True
             for multiple in range(number**2, limit_number, number):
                 numbers_list[multiple] = False
     for number in range(math.isqrt(limit_number) + 1, limit_number):
-        if numbers_list[number] == None:
+        if numbers_list[number] is None:
             numbers_list[number] = True
     return [number for number in range(limit_number) if numbers_list[number]]
+
 
 if __name__ == "__main__":
     print(primes_less_than(100))
